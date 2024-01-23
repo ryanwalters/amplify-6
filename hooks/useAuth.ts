7ca42662deba6c288e1532b12143a424babd8038
Ryan Walters <ryan@ryanwalters.dev>
2024-01-22 22:47:40 -0600

    initial commit

---
+++ hooks/useAuth.ts
@@ -0,0 +1,19 @@
+import { signIn } from 'aws-amplify/auth';
+import { Amplify } from 'aws-amplify';
+
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID!,
+      userPoolClientId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_CLIENT_ID!,
+    }
+  }
+}, {
+  ssr: true,
+});
+
+export function useAuth() {
+  return {
+    signIn,
+  }
+}
